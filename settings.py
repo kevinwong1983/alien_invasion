ec39b10608b6928f3c4de92f17b3c8d0cc95742b
Kevin Wong <kevin.wong@philips.com>
2022-05-29 23:13:49 +0200

    add aliens

--- settings.py
+++ settings.py
@@ -10,10 +10,17 @@ class Settings:
         
         # Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
         
         # Bullet settings
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_speed = 1.5
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+        
+        # Alien settings
+        self.alien_speed = 30.0
+        self.fleet_drop_speed = 10
+        # Fleet direction of 1 represent right; -1 represent left
+        self.fleet_direction = 1
